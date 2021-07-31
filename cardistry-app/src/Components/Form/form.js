f373e7df73d024da5994bec7ff34cb3412c3566b
emmaline01 <emmaline.mai@gmail.com>
2021-07-31 13:55:57 -0700

    added bootstrap and started playing with the form UI

--- cardistry-app/src/Components/Form/form.js
+++ cardistry-app/src/Components/Form/form.js
@@ -12,12 +12,18 @@ export const Form = ({inputMove, onFormChange, onFormSubmit})=> {
         onFormSubmit()
     }
 
+    //col-md-# doesn't work for text input??? but col does?
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input type='text' required value={inputMove} onChange={handleNameChange}></input>
-                <input type='submit'></input>
-            </form>
+            <div class="container">
+                <form onSubmit={handleSubmit}>
+                    <div class="row">
+                        <input type='text' placeholder="Move name" required value={inputMove} onChange={handleNameChange} class="form-control col"></input>
+                        <input type='text' class="form-control col"></input>
+                        <button type="submit" class="btn btn-secondary col-md-1">Submit</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
